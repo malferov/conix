f59a7db0d8039a68706ef10d50134228df2f02da
Maxim Alferov <malferov44@gmail.com>
2025-08-17 22:48:19 +0200

    infra

---
+++ infra/backend.hcl
@@ -0,0 +1,4 @@
+workspaces { name = "conixsite" }
+hostname     = "app.terraform.io"
+organization = "conixone"
+token        = "{{ be_token }}"
---
+++ infra/tls.tf
@@ -0,0 +1,36 @@
+variable "domain" {}
+variable "email" {}
+
+resource "tls_private_key" "private_key" {
+  algorithm   = "RSA"
+  ecdsa_curve = "P256"
+}
+
+resource "acme_registration" "reg" {
+  account_key_pem = tls_private_key.private_key.private_key_pem
+  email_address   = var.email
+}
+
+# trigger G00
+resource "acme_certificate" "cert" {
+  account_key_pem           = acme_registration.reg.account_key_pem
+  common_name               = var.domain
+  dns_challenge {
+    provider = "digitalocean"
+    config = {
+      DO_AUTH_TOKEN = var.do_token
+      DO_POLLING_INTERVAL = 3
+      DO_PROPAGATION_TIMEOUT = 60
+    }
+  }
+}
+
+output "crt" {
+  sensitive = true
+  value     = base64encode("${acme_certificate.cert.certificate_pem}${acme_certificate.cert.issuer_pem}")
+}
+
+output "key" {
+  sensitive = true
+  value     = base64encode(acme_certificate.cert.private_key_pem)
+}
